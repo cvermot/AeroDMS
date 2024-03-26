289b287f346ba04aa1429527b0b73bc9da1b60db
Clément Vermot <clement.vermot@gmail.com>
2024-03-26 22:32:43 +0100

    Version 3.6

--- src/AeroDms.cpp
+++ src/AeroDms.cpp
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 AeroDms::AeroDms(QWidget* parent) :QMainWindow(parent)
 {
     QApplication::setApplicationName("AeroDms");
-    QApplication::setApplicationVersion("3.5");
+    QApplication::setApplicationVersion("3.6");
     QApplication::setWindowIcon(QIcon("./ressources/shield-airplane.svg"));
     mainTabWidget = new QTabWidget(this);
     setCentralWidget(mainTabWidget);
