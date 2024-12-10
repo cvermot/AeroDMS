c44d7471bae2d2bf0b629fbd16b6c77d84dba096
Clément Vermot <clement.vermot@gmail.com>
2024-12-10 20:50:58 +0100

    Amélioration icone application

--- AeroDms/resource.h
+++ AeroDms/resource.h
@@ -13,6 +13,6 @@
 #endif
 #endif
 
-#define VER_PRODUCTVERSION          "7.2.2"
+#define VER_PRODUCTVERSION          "7.3"
 #define VER_PRODUCTVERSION_STR      VER_PRODUCTVERSION "\0"
 #define VER_PRODUCTNAME_STR         "AeroDMS"
