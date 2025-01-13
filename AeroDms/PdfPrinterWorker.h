880782173269f93cd2dd30d6308cc8561571e8ad
Clément Vermot <clement.vermot@gmail.com>
2025-01-13 20:24:39 +0100

    Mise à jour date copyright

--- AeroDms/PdfPrinterWorker.h
+++ AeroDms/PdfPrinterWorker.h
@@ -1,6 +1,6 @@
 /******************************************************************************\
 <AeroDms : logiciel de gestion compta section aéronautique>
-Copyright (C) 2023-2024 Clément VERMOT-DESROCHES (clement@vermot.net)
+Copyright (C) 2023-2025 Clément VERMOT-DESROCHES (clement@vermot.net)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
